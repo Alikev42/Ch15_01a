4597c3b3712624fd067b0753dd6601af3ab08f4b
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-05-30 17:42:43 -0700

    Hulk Smash

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+/* Kevin R. Salger */
+/* IS657, Chapter 15 sample code */
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.box}>
+    <Text style={styles.boxText}>I'm in a box.</Text>
+    </View>
     </View>
   );
 }
@@ -18,4 +23,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  box:
+  {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray"
+  },
+
+  boxText:
+  {
+    color: "darkslategray",
+    fontWeight: "bold"
+  }
 });
